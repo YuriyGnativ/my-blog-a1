eaf0e0ab14d6b85fd924bfa6edcbf81677df8462
dmytro.shklianka <dmytro.shklianka.work@gmail.com>
2022-06-28 00:46:45 +0400

    changed package.json and indexjs 1

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res, next) => {
   res.send("hello");
 });
 
-app.listen(5000);
+app.listen(PORT, () => {
+  console.log(`Server's running at port ${PORT}`);
+});
